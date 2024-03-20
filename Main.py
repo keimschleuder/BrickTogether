d6decdcad05646786dca0e8026a0f0b8dc8dbed0
Niklas <138971666+keimschleuder@users.noreply.github.com>
2024-03-20 18:11:01 +0100

    Made the Bot work

--- Main.py
+++ Main.py
@@ -1,2 +1,15 @@
+from Token.Token import TOKEN as token
 import discord
-from Token.Token import TOKEN as t
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+activity = discord.Activity(type=discord.ActivityType.watching, name="dir zu")
+bot = commands.Bot(command_prefix = "!", activity=activity, intents = intents, status=discord.Status.online)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+bot.run(token)
